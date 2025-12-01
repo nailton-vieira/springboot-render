6092815be829070374bb7bbe00e36c98fbd2858f
nailton-vieira <nailtonvieira@gmail.com>
2025-12-01 13:45:27 -0300

    Enviando base ao repo

---
+++ src/main/java/com/nailton/spring_render/SpringRenderApplication.java
@@ -0,0 +1,13 @@
+package com.nailton.spring_render;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nailton/spring_render/SpringRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailton.spring_render;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
